a457d5d7e4418928041bac3a4d89f0428f68e665
FirdausyTama <atama.firdausy@gmail.com>
2024-11-20 21:41:09 +0700

    frist commit

---
+++ app/src/main/java/com/example/simpleviewmodel/ui/view/FormMahasiswaView.kt
@@ -0,0 +1,100 @@
+package com.example.simpleviewmodel.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FormMahasiswaView(
+    modifier: Modifier = Modifier,
+    listGender: List<String>,
+    onSubmitClick: (MutableList<String>) -> Unit
+
+                      ) {
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var noTelpon by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var memilihJk by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(
+        nama, memilihJk, alamat
+    )
+
+
+    Column (modifier = modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment =  Alignment.CenterHorizontally
+
+    ){
+        Text("Biodata",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,)
+        Spacer(Modifier .padding(16.dp))
+        TextField(
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            value = nama,
+            onValueChange = {nama = it},
+            label = { Text("nama") },
+            placeholder = { Text("Masukan Nama Anda") }
+        )
+        Row (verticalAlignment = Alignment.CenterVertically){
+            listGender.forEach { item ->
+                Row (verticalAlignment = Alignment.CenterVertically){ RadioButton(
+                    selected = memilihJk == item,
+                    onClick = {
+                        memilihJk = item
+                    }
+                )
+                    Text(item)
+                }
+            }
+        }
+        TextField(
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            value = email,
+            onValueChange = {email = it},
+            label = { Text("email") },
+            placeholder = { Text("Masukan Email Anda") }
+        )
+        TextField(
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            value = noTelpon,
+            onValueChange = {noTelpon = it},
+            label = { Text("No Tlp") },
+            placeholder = { Text("Masukan No Tlp") }
+        )
+        TextField(
+            modifier = Modifier.fillMaxWidth().padding(5.dp),
+            value = alamat,
+            onValueChange = {alamat = it},
+            label = { Text("Alamat") },
+            placeholder = { Text("Masukan Alamat Anda") }
+        )
+
+        Button (onClick = {
+            onSubmitClick(listData)
+        }) {
+            Text("Submit")
+        }
+    }
+}
+
