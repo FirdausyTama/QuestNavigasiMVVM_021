4b275ee5ae6cb890baf6d6c0286dc642b1bae4f0
FirdausyTama <atama.firdausy@gmail.com>
2024-11-20 22:02:21 +0700

    membvuat viewmodel mahasiswa dan navhost

--- app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
@@ -1,7 +1,13 @@
 package com.example.simpleviewmodel
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.simpleviewmodel.viewmodel.MahasiswaViewModel
 
 enum class Halaman{
     Form,
@@ -11,4 +17,6 @@ enum class Halaman{
 @Composable
 fun Navigasi(
     modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
 ){}
